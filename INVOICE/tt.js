b0ec367c168cea21912dcf84bfc1bcb0f07057bc
ZakiTes <120137737+ZakiTes@users.noreply.github.com>
2025-04-28 19:44:53 -0600

    Add files via upload

---
+++ INVOICE/tt.js
@@ -0,0 +1,6 @@
+// Set Header Content
+const companyLogo = 'logo.png'; // Replace '...' with your base64 image data for the logo
+const companyName = 'One Day Roofing & Siding Ltd.';
+const address = '122 pineset place NE, Calagary';
+const email = 'habtehani@yahoo.com';
+const phone = '+123 456 7890';
